5a63bb65688a9285fbbbad9b07187de1ade17bf5
Andres Almiray <aalmiray@gmail.com>
2021-11-09 12:20:36 +0100

    Reste avatars

---
+++ resources/reset-avatars.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cp ${BASEDIR}/images/duke-avatar.png ${BASEDIR}/images/avatars/Barry_Levine.png
+cp ${BASEDIR}/images/duke-avatar.png ${BASEDIR}/images/avatars/Calvin_Austin.png
+cp ${BASEDIR}/images/duke-avatar.png ${BASEDIR}/images/avatars/Flavia_Coelho.png
+cp ${BASEDIR}/images/duke-avatar.png ${BASEDIR}/images/avatars/Jayson_Raymond.png
+cp ${BASEDIR}/images/duke-avatar.png ${BASEDIR}/images/avatars/Jeff_Cobb.png
+cp ${BASEDIR}/images/duke-avatar.png ${BASEDIR}/images/avatars/Paul_Lipton.png 
+cp ${BASEDIR}/images/duke-avatar.png ${BASEDIR}/images/avatars/Qusay_Mahmoud.png
+cp ${BASEDIR}/images/duke-avatar.png ${BASEDIR}/images/avatars/Srikanth_Raju.png
+cp ${BASEDIR}/images/duke-avatar.png ${BASEDIR}/images/avatars/davidgeary.png
